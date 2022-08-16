d680c5bc0935fb16f6bce1bd09311636a18c5688
noon-ghe <naderghaliany@gmail.com>
2022-08-16 21:32:15 +0430

    UIsmartButton.cs and GameManager.cs added (empty)

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,13 @@
+// Change Default scripts on Editor\Data\Resources\ScriptTemplates
+
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class GameManager: MonoBehaviour
+{
+   void Start()
+   {
+      
+   }
+}
