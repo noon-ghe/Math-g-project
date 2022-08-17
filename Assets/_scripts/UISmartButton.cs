9c49d7580e33751a85a9296754c6c58afd276a72
noon-ghe <naderghaliany@gmail.com>
2022-08-17 19:47:49 +0430

    UISmartButton.cs configured

--- Assets/_scripts/UISmartButton.cs
+++ Assets/_scripts/UISmartButton.cs
@@ -1,13 +1,93 @@
-// Change Default scripts on Editor\Data\Resources\ScriptTemplates
-
-using System.Collections.Generic;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UISmartButton: MonoBehaviour
+enum ButtonState
 {
-   void Start()
-   {
-      
-   }
+    KeyboardNumber,
+    Enter,
+    Clear,
+    LevelSelector,
+
+    Back,
+    StartGame,
+    SelectLevel,
+
+    DisplayLevels
+    //...
+}
+
+public class UISmartButton : MonoBehaviour
+{
+    [SerializeField] private ButtonState state;
+    private UserData _userData;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(MyButtonClicked);
+    }
+
+    private void OnEnable()
+    {
+        Transition.OnTransition += SetButtonState;
+        if (state == ButtonState.KeyboardNumber)
+        {
+            GetComponent<Button>().interactable = true;
+        }
+        //Riddle.OnRiddleStateChanged += SetButtonState;
+    }
+
+    private void OnDisable()
+    {
+        //Riddle.OnRiddleStateChanged -= SetButtonState;
+    }
+
+
+    async void MyButtonClicked()
+    {
+        switch (state)
+        {
+            case ButtonState.KeyboardNumber:
+                Riddle.I.UpdateUserAnswer(GetComponentInChildren<TextMeshProUGUI>().text);
+                break;
+            case ButtonState.Enter:
+                Riddle.I.CheckAnswer();
+                break;
+            case ButtonState.Clear:
+                Riddle.I.ClearInputField();
+                break;
+            case ButtonState.LevelSelector:
+                Riddle.I.SelectLevel(int.Parse(GetComponentInChildren<TextMeshProUGUI>().text) - 1);
+                break;
+            case ButtonState.Back:
+                GameManager.I.Back();
+                break;
+            case ButtonState.StartGame:
+                GameManager.I.UpdateGameState(GameStates.Game);
+                break;
+            case ButtonState.DisplayLevels:
+                GameManager.I.UpdateGameState(GameStates.Levels);
+                break;
+            case ButtonState.SelectLevel:
+                GameManager.I.UpdateGameState(GameStates.Game);
+                await Task.Delay(
+                    TimeSpan.FromSeconds(Transition.I
+                        .GetHalfOfTransitionTime())); //for sure that gamestate updated and Riddle are active
+                Riddle.I.SelectLevel(int.Parse(GetComponentInChildren<TextMeshProUGUI>().text) - 1);
+                break;
+        }
+    }
+
+    public void SetButtonState(bool bstate)
+    {
+        if (state != ButtonState.LevelSelector)
+        {
+            //GetComponent<Button>().interactable = bstate;
+        }
+
+    }
 }
